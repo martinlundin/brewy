cb20233d7973d45674ce7cb15dd1d4d8f8a5f34f
Martin Lundin <mlundin1994@gmail.com>
2019-10-27 09:18:08 +0100

    Added avatar to profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import FileUploader from "react-firebase-file-uploader";
 
 // Mui
 import Container from '@material-ui/core/Container';
@@ -7,17 +8,70 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
 // Local
 import { AuthContext } from '../util/auth';
 import { StatusContext } from '../util/status';
 import firebase from '../util/firebase';
 import Loader from '../components/Loader';
+import generatePattern from '../util/pattern'
+
+const useStyles = makeStyles((theme) => ({
+  avatarWrap: {
+    display: 'flex',
+    width: '100px',
+    height: '100px',
+    margin: '0 auto',
+    alignItems: 'flex-end',
+    background: 'transparent',
+  },
+  avatar: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    background: 'rgba(0,0,0,0.2)',
+  },
+  avatarText: {
+    width: '100%',
+    background: 'rgba(0,0,0,0.2)',
+    color: theme.palette.text.primary,
+    position: 'absolute',
+  }
+}));
 
 function Profile({ history }) {
+  const classes = useStyles();
+
   const [currentUser, setCurrentUser] = React.useContext(AuthContext);
   const [status, setStatus] = React.useContext(StatusContext);
   const [displayName, setDisplayName] = React.useState('');
+  const [avatar, setAvatar] = React.useState('');
+
+  React.useEffect(() => {
+    if (currentUser && currentUser.profile) {
+      setDisplayName(currentUser.profile.displayName);
+      setAvatar(currentUser.profile.avatar);
+    } else{
+      setAvatar(generatePattern());
+    }
+  }, [currentUser]);
+
+  const handleProgress = progress => console.log(progress)
+  const handleUploadError = error => {
+    console.error(error);
+  };
+  const handleUploadSuccess = filename => {
+    console.log('success upload')
+    firebase
+      .storage()
+      .ref("images")
+      .child(filename)
+      .getDownloadURL()
+      .then(url => {setAvatar(url); console.log('successurl', url)});
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -26,6 +80,7 @@ function Profile({ history }) {
     // Send profile to firebase
     const userProfile = {
         displayName,
+        avatar,
     }
     firebase.firestore().collection('users').doc(currentUser.uid).set(userProfile)
     .then(() => {
@@ -45,19 +100,27 @@ function Profile({ history }) {
     })
   };
 
-  React.useEffect(() => {
-    if (currentUser && currentUser.profile) {
-      setDisplayName(currentUser.profile.displayName);
-    }
-  }, [currentUser]);
-
   return (
     <Container>
       <Typography variant="h1">Profile</Typography>
       <form onSubmit={submitHandler}>
-        <Typography variant="h4">
-          {currentUser.phoneNumber}
-        </Typography>
+        <label>
+          <Avatar className={classes.avatarWrap}>
+            <Avatar className={classes.avatar} src={avatar} alt="avatar">
+            </Avatar>
+            <Typography className={classes.avatarText}>Upload</Typography>
+          </Avatar>
+          <FileUploader
+            accept="image/*"
+            name="avatar"
+            randomizeFilename
+            hidden
+            storageRef={firebase.storage().ref("images")}
+            onUploadError={handleUploadError}
+            onUploadSuccess={handleUploadSuccess}
+            onProgress={handleProgress}
+          />
+        </label>
         <TextField
           name="displayName"
           label="Display Name"
